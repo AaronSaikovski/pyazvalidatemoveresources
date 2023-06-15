91bc6ce1b21815d161eee6bfb2318afaa0f20f63
AaronSaikovski <asaikovski@outlook.com>
2023-06-15 15:03:01 +1000

    minor

--- azure_helper/auth_helper.py
+++ azure_helper/auth_helper.py
@@ -1,6 +1,7 @@
 """
 Azure Auth Helper
 """
+from azure.core.credentials import AccessToken as AccessToken
 from azure.identity import AzureCliCredential
 
 import common.constants as constants
@@ -8,9 +9,15 @@ import common.constants as constants
 # ******************************************************************************** #
 
 
-def get_az_cached_access_token() -> object:
+def get_az_cached_access_token() -> AccessToken:
     """
     Gets the cached access token from the AZ CLI session token
+    """
+
+    """Gets the cached access token from the AZ CLI session token
+
+    Returns:
+        AccessToken: Access token from scope
     """
     credential = AzureCliCredential()
     access_token = credential.get_token(constants.AZURE_MGMT_URL)
